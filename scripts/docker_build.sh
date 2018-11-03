9eead31e25adb345fa66436f33124e10ba209464
Samuel Walladge <samuel@swalladge.id.au>
2018-11-03 11:39:32 +1030

    build python37 wheels in docker

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -7,7 +7,7 @@ set -e -x
 
 # code from https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
 # Compile wheels
-for PYBIN in /opt/python/{cp27-cp27m,cp27-cp27mu,cp33-cp33m,cp34-cp34m,cp35-cp35m,cp36-cp36m}/bin; do
+for PYBIN in /opt/python/{cp27-cp27m,cp27-cp27mu,cp34-cp34m,cp35-cp35m,cp36-cp36m,cp37-cp37m}/bin; do
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     cd /io/
     "${PYBIN}/python" setup.py bdist_wheel -d /wheelhouse/
