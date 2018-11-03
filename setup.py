627fc70ce45e20e18a5977d9cf476fac5592a7a7
Samuel Walladge <samuel@swalladge.id.au>
2018-11-03 11:19:11 +1030

    remove attempt to download enet sources
    (sources are checked into the repository directly)

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ import re
 source_files = ["enet.pyx"]
 
 _enet_files = glob.glob("enet/*.c")
+source_files.extend(_enet_files)
+
 lib_version = "1.3.13"
 package_version = lib_version + '.post6'
 
@@ -31,55 +33,6 @@ class build_ext(_build_ext):
         extra_args = sys.argv[2:]
         run_setup(os.path.join(os.getcwd(), "setup.py"), ['build_py'] + extra_args)
 
-
-if not _enet_files:
-    import tarfile
-    try:
-        import urllib.request as urllib_request
-    except ImportError:
-        import urllib as urllib_request
-
-    enet_dir = "enet-%s/" % lib_version
-    enet_file = "enet-%s.tar.gz" % lib_version
-    enet_url = "http://enet.bespin.org/download/%s" % enet_file
-
-    if not os.path.isfile(enet_file):
-        print("Downloading enet")
-
-        # begin code shamelessly stolen from StackOverflow
-        # http://stackoverflow.com/a/13895723/2730399
-        # Answer by https://stackoverflow.com/users/4279/j-f-sebastian
-        def reporthook(blocknum, blocksize, totalsize):
-            readsofar = blocknum * blocksize
-            if totalsize > 0:
-                percent = readsofar * 1e2 / totalsize
-                s = "\r%5.1f%% %*d / %d" % (percent, len(str(totalsize)),
-                                            readsofar, totalsize)
-                sys.stderr.write(s)
-                if readsofar >= totalsize:  # near the end
-                    sys.stderr.write("\n")
-            else:  # total size is unknown
-                sys.stderr.write("read %d\n" % (readsofar, ))
-
-        urllib_request.urlretrieve(enet_url, enet_file, reporthook)
-        print("Finished downloading enet")
-        # end code shamelessly stolen from StackOverflow
-
-    print("Unpacking enet")
-    tar = tarfile.open(enet_file)
-
-    l = len(enet_dir)
-    members = []
-    for member in tar.getmembers():
-        if member.path.startswith(enet_dir):
-            member.path = member.path[l:]
-            members.append(member)
-
-    tar.extractall('enet', members=members)
-    tar.close()
-    print("Finished unpacking enet")
-
-source_files.extend(_enet_files)
 
 define_macros = [('HAS_POLL', None), ('HAS_FCNTL', None),
                  ('HAS_MSGHDR_FLAGS', None), ('HAS_SOCKLEN_T', None)]
